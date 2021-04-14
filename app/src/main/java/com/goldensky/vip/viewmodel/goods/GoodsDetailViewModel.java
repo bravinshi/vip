4d83ee4444a0be26069c154cebdbb0e00f22f301
bravin <470726148@qq.com>
2021-04-14 15:05:29 +0800

    引入屏幕适配

---
+++ app/src/main/java/com/goldensky/vip/viewmodel/goods/GoodsDetailViewModel.java
@@ -0,0 +1,13 @@
+package com.goldensky.vip.viewmodel.goods;
+
+import com.goldensky.vip.viewmodel.PublicViewModel;
+
+/**
+ * @author bravin
+ * @version 1.0
+ * 创建日期：2021/4/14 14:23
+ * 包名： com.goldensky.vip.viewmodel.goods
+ * 类说明：
+ */
+public class GoodsDetailViewModel extends PublicViewModel {
+}
