c0aea1e0f856615ffd46cc1760def6715cb0723e
bravin <470726148@qq.com>
2021-04-22 11:51:28 +0800

    文章详情

--- app/src/main/java/com/goldensky/vip/activity/article/ArticleDetailActivity.java
+++ app/src/main/java/com/goldensky/vip/activity/article/ArticleDetailActivity.java
@@ -1,5 +1,14 @@
 package com.goldensky.vip.activity.article;
 
+import android.os.Bundle;
+import android.view.View;
+
+import com.goldensky.framework.viewmodel.BaseViewModel;
+import com.goldensky.vip.R;
+import com.goldensky.vip.base.activity.BaseActivity;
+import com.goldensky.vip.databinding.ActivityArticleDetailBinding;
+import com.gyf.immersionbar.ImmersionBar;
+
 /**
  * @author bravin
  * @version 1.0
@@ -7,5 +16,21 @@ package com.goldensky.vip.activity.article;
  * 包名： com.goldensky.vip.activity.article
  * 类说明：
  */
-public class ArticleDetailActivity {
+public class ArticleDetailActivity extends BaseActivity<ActivityArticleDetailBinding, BaseViewModel> {
+    @Override
+    public void onFinishInit(Bundle savedInstanceState) {
+        ImmersionBar.with(this).statusBarDarkFont(true)
+                .statusBarView(mBinding.vStatusBar).init();
+        mBinding.tabBar.setBackListener(v -> ArticleDetailActivity.this.finish());
+    }
+
+    @Override
+    public void observe() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_article_detail;
+    }
 }
