81befb421526deafe63217cd35584d2358539409
bravin <470726148@qq.com>
2021-04-22 11:50:11 +0800

    文章详情

---
+++ app/src/main/java/com/goldensky/vip/activity/article/ArticleDetailActivity.java
@@ -0,0 +1,11 @@
+package com.goldensky.vip.activity.article;
+
+/**
+ * @author bravin
+ * @version 1.0
+ * 创建日期：2021/4/22 11:22
+ * 包名： com.goldensky.vip.activity.article
+ * 类说明：
+ */
+public class ArticleDetailActivity {
+}
