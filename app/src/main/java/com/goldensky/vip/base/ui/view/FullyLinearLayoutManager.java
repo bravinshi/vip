02d32d2b9bae120694603af96fcd6209687338c3
bravin <470726148@qq.com>
2021-04-23 11:52:13 +0800

    滑动优化

--- app/src/main/java/com/goldensky/vip/base/ui/view/FullyLinearLayoutManager.java
+++ app/src/main/java/com/goldensky/vip/base/ui/view/FullyLinearLayoutManager.java
@@ -42,53 +42,55 @@ public class FullyLinearLayoutManager extends LinearLayoutManager {
     public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state,
                           int widthSpec, int heightSpec) {
 
-        final int widthMode = View.MeasureSpec.getMode(widthSpec);
-        final int heightMode = View.MeasureSpec.getMode(heightSpec);
-        final int widthSize = View.MeasureSpec.getSize(widthSpec);
-        final int heightSize = View.MeasureSpec.getSize(heightSpec);
+        super.onMeasure(recycler, state, widthSpec, heightSpec);
 
-        Log.i("RetrofitLog", "onMeasure called. \nwidthMode " + widthMode
-                + " \nheightMode " + heightSpec
-                + " \nwidthSize " + widthSize
-                + " \nheightSize " + heightSize
-                + " \ngetItemCount() " + getItemCount());
-
-        int width = 0;
-        int height = 0;
-        for (int i = 0; i < getItemCount(); i++) {
-            measureScrapChild(recycler, i,
-                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                    mMeasuredDimension);
-
-            if (getOrientation() == HORIZONTAL) {
-                width = width + mMeasuredDimension[0];
-                if (i == 0) {
-                    height = mMeasuredDimension[1];
-                }
-            } else {
-                height = height + mMeasuredDimension[1];
-                if (i == 0) {
-                    width = mMeasuredDimension[0];
-                }
-            }
-        }
-        switch (widthMode) {
-            case View.MeasureSpec.EXACTLY:
-                width = widthSize;
-            case View.MeasureSpec.AT_MOST:
-            case View.MeasureSpec.UNSPECIFIED:
-        }
-
-        switch (heightMode) {
-            case View.MeasureSpec.EXACTLY:
-                height = heightSize;
-            case View.MeasureSpec.AT_MOST:
-            case View.MeasureSpec.UNSPECIFIED:
-        }
-
-        Log.d("RetrofitLog", "width " + width + " height " + height);
-        setMeasuredDimension(width, height);
+//        final int widthMode = View.MeasureSpec.getMode(widthSpec);
+//        final int heightMode = View.MeasureSpec.getMode(heightSpec);
+//        final int widthSize = View.MeasureSpec.getSize(widthSpec);
+//        final int heightSize = View.MeasureSpec.getSize(heightSpec);
+//
+//        Log.i("RetrofitLog", "onMeasure called. \nwidthMode " + widthMode
+//                + " \nheightMode " + heightSpec
+//                + " \nwidthSize " + widthSize
+//                + " \nheightSize " + heightSize
+//                + " \ngetItemCount() " + getItemCount());
+//
+//        int width = 0;
+//        int height = 0;
+//        for (int i = 0; i < getItemCount(); i++) {
+//            measureScrapChild(recycler, i,
+//                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+//                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+//                    mMeasuredDimension);
+//
+//            if (getOrientation() == HORIZONTAL) {
+//                width = width + mMeasuredDimension[0];
+//                if (i == 0) {
+//                    height = mMeasuredDimension[1];
+//                }
+//            } else {
+//                height = height + mMeasuredDimension[1];
+//                if (i == 0) {
+//                    width = mMeasuredDimension[0];
+//                }
+//            }
+//        }
+//        switch (widthMode) {
+//            case View.MeasureSpec.EXACTLY:
+//                width = widthSize;
+//            case View.MeasureSpec.AT_MOST:
+//            case View.MeasureSpec.UNSPECIFIED:
+//        }
+//
+//        switch (heightMode) {
+//            case View.MeasureSpec.EXACTLY:
+//                height = heightSize;
+//            case View.MeasureSpec.AT_MOST:
+//            case View.MeasureSpec.UNSPECIFIED:
+//        }
+//
+//        Log.d("RetrofitLog", "width " + width + " height " + height);
+//        setMeasuredDimension(width, height);
     }
 
     private void measureScrapChild(RecyclerView.Recycler recycler, int position, int widthSpec,
