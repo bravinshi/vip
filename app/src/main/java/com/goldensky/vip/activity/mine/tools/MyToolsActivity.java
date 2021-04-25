b540456c0dd54f220dffb1047efa15fa41e556a5
You jiawang <m18510628372@163.com>
2021-04-25 17:19:29 +0800

    我的工具布局搭建

---
+++ app/src/main/java/com/goldensky/vip/activity/mine/tools/MyToolsActivity.java
@@ -0,0 +1,29 @@
+package com.goldensky.vip.activity.mine.tools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.goldensky.vip.R;
+import com.goldensky.vip.base.activity.BaseActivity;
+import com.goldensky.vip.databinding.ActivityMyToolsBinding;
+import com.goldensky.vip.viewmodel.PublicViewModel;
+
+public class MyToolsActivity extends BaseActivity<ActivityMyToolsBinding, PublicViewModel> {
+
+
+    @Override
+    public void onFinishInit(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void observe() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_my_tools;
+    }
+}
